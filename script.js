c36561a746f46d142c00247c226e082194fcecf0
abdrhman112233 <109982638+abdrhman112233@users.noreply.github.com>
2022-08-19 11:02:19 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
+function BmI ( weight,height){
+  let resutl =   weight / (height * height)
+return resutl;
+
+
+}
+console.log(BmI(10,10));
+console.log(BmI(10,10));
+console.log(BmI(10,10));
+console.log(BmI(10,10));
+console.log(BmI(10,10));
+
+
+function Status (BmI){
+
+    if (BmI < 18.5) {
+    document.getElementById("result").style.color="orange";
+        return"نقص بالوزن ";
+
+    
+    }else if (BmI>=18.5 && BmI<=25 ) {
+      document.getElementById("result").style.color="green";
+    return "وزنك صحي";
+
+    
+    }else if(BmI>=25) {
+      document.getElementById("result").style.color="orange";
+    return "لديك زياده بالوزن";
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+function calculate() {
+let weight= document.getElementById("weight").value;
+let height= document.getElementById("height").value;
+let bmi = BmI(weight,height)
+let desc = Status (bmi);
+let div = document.getElementById('result');
+ div.innerText = bmi + " == " + desc;
+
+}
+
+
 
